9151ffc424bf6af8e96a950bbf0bfe1ad2b19203
Mark Steve Samson <hello@marksteve.com>
2016-04-01 18:22:39 +0800

    Add color

--- webpack/js/main.js
+++ webpack/js/main.js
@@ -1,15 +1,24 @@
 import 'blissfuljs'
+import tinycolor from 'tinycolor2'
 
-let backgrounds = $('.backgrounds')
+let accentColor, backgrounds = $('.backgrounds')
 
 $$('[data-background]').forEach(function(el) {
   let background = $('.' + el.dataset.background, backgrounds)
   el._.events({
     'mouseenter': function() {
-      background._.transition({opacity: 1})
+      el._.transition({color: accentColor})
+      background._.transition({opacity: 0.5})
     },
     'mouseleave': function() {
+      el._.transition({color: 'white'})
       background._.transition({opacity: 0})
     },
   })
 })
+
+function randomColor() {
+  accentColor = tinycolor.random().toHexString()
+  backgrounds._.style({backgroundColor: accentColor})
+}
+setInterval(randomColor, 500)
